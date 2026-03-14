1765d30991c56cfd78e8cdc0512745a536f0839e
JOHN MAR DUMOPOL <johnmardumopol98@gmail.com>
2026-03-14 12:08:06 +0800

    Delete activity #2/activity #3 directory

--- activity #2/activity #3/modules.js
+++
@@ -1,9 +0,0 @@
-export async function fetchData(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-}
